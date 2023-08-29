895c37b56dff7599735396f01c08c4ffec63ec51
João Victor <65056371+devwckd@users.noreply.github.com>
2023-08-29 20:14:57 -0300

    feat: quadratic and cubic ease equations (#3)

--- tid-core/src/main/java/me/devwckd/tid/interpolation/EasingInterpolator.java
+++ tid-core/src/main/java/me/devwckd/tid/interpolation/EasingInterpolator.java
@@ -1,14 +1,67 @@
 package me.devwckd.tid.interpolation;
 
+
+/**
+ * <a href="https://github.com/mattdesl/cisc226game/blob/master/SpaceGame/src/space/engine/easing/Easing.java">Equations Source</a>
+ */
 public interface EasingInterpolator extends Interpolator {
 
+    /**
+     * Linear - no tweening
+     */
     EasingInterpolator LINEAR = (first, last, currentFrame, totalFrames) -> {
         double change = last - first;
         return change * currentFrame / totalFrames + first;
     };
 
-    EasingInterpolator EASE_IN = (first, last, currentFrame, totalFrames) -> {
+    /**
+     * Quadratic Ease In
+     */
+    EasingInterpolator EASE_IN_QUAD = (first, last, currentFrame, totalFrames) -> {
         double change = last - first;
         return change * (currentFrame /= totalFrames) * currentFrame + first;
+    };
+
+    /**
+     * Quadratic Ease Out
+     */
+    EasingInterpolator EASE_OUT_QUAD = (first, last, currentFrame, totalFrames) -> {
+        double change = last - first;
+        return -change * (currentFrame /= totalFrames) * (currentFrame - 2) + first;
+    };
+
+    /**
+     * Quadratic Ease In & Out
+     */
+    EasingInterpolator EASE_IN_OUT_QUAD = (first, last, currentFrame, totalFrames) -> {
+        double change = last - first;
+        if ((currentFrame /= totalFrames / 2) < 1) return change / 2 * currentFrame * currentFrame + first;
+        return -change / 2 * ((--currentFrame) * (currentFrame - 2) - 1) + first;
+    };
+
+    /**
+     * Cubic Ease In
+     */
+    EasingInterpolator EASE_IN_CUBIC = (first, last, currentFrame, totalFrames) -> {
+        double change = last - first;
+        return change * (currentFrame /= totalFrames) * currentFrame * currentFrame + first;
+    };
+
+    /**
+     * Cubic Ease Out
+     */
+    EasingInterpolator EASE_OUT_CUBIC = (first, last, currentFrame, totalFrames) -> {
+        double change = last - first;
+        return change * ((currentFrame = currentFrame / totalFrames - 1) * currentFrame * currentFrame + 1) + first;
+    };
+
+    /**
+     * Cubic Ease In & Out
+     */
+    EasingInterpolator EASE_IN_OUT_CUBIC = (first, last, currentFrame, totalFrames) -> {
+        double change = last - first;
+        if ((currentFrame /= totalFrames / 2) < 1)
+            return change / 2 * currentFrame * currentFrame * currentFrame + first;
+        return change / 2 * ((currentFrame -= 2) * currentFrame * currentFrame + 2) + first;
     };
 }
